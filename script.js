57947d32ac444cd5726a71be10506eb8bcb7a737
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2025-06-29 11:23:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
+
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
+
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age); 
-	this.jobTitle = jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
 Employee.prototype = Object.create(Person.prototype);
@@ -23,3 +25,4 @@ Employee.prototype.jobGreet = function () {
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
